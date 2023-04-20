8bf33d88b8169ddb17a613f73f2960097b66e268
Mario Patronelli <mar.ioepatronelli@gmail.com>
2023-04-19 23:37:02 -0300

    Version2. de facudo. subido por mi por problemas con git

--- poker.ts
+++ poker.ts
@@ -1,82 +1,59 @@
-/*enums = Enumerables - Asigna numeros a unas constantes */
-enum Palo {
-  Picas,
-  Diamantes,
-  Corazones,
-  Treboles,
-}
+import * as readlineSync from "readline-sync"; // formato typscrypt
+import { JuegoDeCasino } from "./JuegoDeCasinoAbstract";
 
-enum RangoCartas {
-  Ass = 1,
-  Dos,
-  Tres,
-  Cuatro,
-  Cinco,
-  Seis,
-  Siete,
-  Ocho,
-  Nueve,
-  Diez,
-  Jota,
-  Reina,
-  Rey,
-}
-
-/* Tipos*/
-export type Carta = {
-  palo: Palo;
-  rango: RangoCartas;
-};
-
-
-export class Mazo {
-  private cartas: Carta[] = [];
-
- /*El constructor inicializa la propiedad "cartas".*/
+export class Poker extends JuegoDeCasino {
   constructor() {
-    /* El bucle exterior recorre los cuatro palos*/
-    for (let palo = 0; palo < 4; palo++) {
-        /* y el bucle interior recorre los 13 rangos de cartas*/
-      for (let rango = 1; rango <= 13; rango++) {
-        /*Crea un objeto del tipo "Carta" con las propiedades "palo" y "rango" de la clase "Mazo".*/
-        this.cartas.push({ palo: palo, rango: rango });
+    super(50, 50000);
+    this.nombre = "Poker";
+  }
+  //---------------------------------------------------------------------------
+  public presentarJuego(): void {
+    console.clear();
+    console.log(`=`.repeat(80));
+    console.log(`Usted a elegido el juego Poker de la Casa, "Mi casa mis reglas"`);
+    console.log(`-`.repeat(80));
+    console.log(` 1- El jugador y la banca reciben cartas.
+    2- No hay empates.
+    3- Si el jugador gana aumenta su dinero y si pierde se le resta.                         
+    4- Probabilidades: 0.7 la Banca y 0.5 el jugador
+    (El valor de apuesta en cada mano es $ ${this.apuestaMinima})`);
+    console.log(`-`.repeat(80));
+    console.log(` Uds. Dispone de $ ${this.dineroDisponible.toLocaleString()} para apostar.`);
+    console.log(`=`.repeat(80));
+  }
+  //---------------------------------------------------------------------------
+  public solicitarCreditos(): number {
+    if (this.dineroDisponible < this.apuestaMinima) {
+      console.log("No tienes suficientes créditos para iniciar el juego."); // Se muestra un mensaje si no hay suficientes créditos
+      return 0; // Se devuelve 0 si no hay suficientes créditos
+    }
+    let respuesta = readlineSync.keyInYNStrict(
+      `Valor unico de apuesta $ ${this.apuestaMinima}. Quiere jugar una Mano`);
+    if (respuesta)
+      return this.apuestaMinima;
+    else
+      throw new Error(`Usted Salio del juego.`);
+  }
+  //---------------------------------------------------------------------------
+  public iniciarJuego(): void {
+    let dinero: number = 0;
+    do {
+      console.log(`-`.repeat(80));
+      dinero = this.solicitarCreditos();
+      if (dinero === 0) /* continuar = false; */ throw new Error(`Operacion cancelada. Fondos insuficientes.`);
+      this.dineroApostado = dinero; 
+      console.log("Repartiendo cartas...");
+      // Generamos un número aleatorio entre 0 y 1 para determinar quién ganó
+      const ganador = Math.floor(Math.random() * 2);
+      if (ganador === 0) {
+        console.log("¡El jugador 1 ganó!");
+        let dineroGanado: number = this.dineroApostado * 2
+        this.pagarApuesta(dineroGanado)
+      } else {
+        this.descontarApuestaPerdida();
+        console.log("¡La banca ganó!");
       }
-    }
-  }
-
-/* Funcion mezclar las cartas de un mazo de manera aleatoria*/
-/* Se itera sobre el arreglo de cartas, desde el último elemento y terminando en el índice 1.
-En cada iteración, se genera un número aleatorio j entre 0 y el índice actual i utilizando Math.random().
-Se intercambia la carta en la posición i con la carta en la posición j.*/
-  mezclar() {
-    for (let i = this.cartas.length - 1; i > 0; i--) { 
-        /*Math.floor() redondea hacia abajo un número decimal y lo convierte en un número entero. 
-        y se multiplica por (i + 1) para generar un número aleatorio entre 0 y i.*/
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.cartas[i], this.cartas[j]] = [this.cartas[j], this.cartas[i]];
-    }
-  }
-/* la funcion dar devuelve la última carta del arreglo "cartas" 
-y elimina esa carta del arreglo utilizando el método pop().*/
-  dar() {
-    return this.cartas.pop();
+      console.log(` Uds. Dispone de $ ${this.dineroDisponible.toLocaleString()} para apostar.`);
+    } while (this.dineroDisponible >= this.apuestaMinima)
   }
 }
-
-/* Probamos */
-
-// const mazo = new Mazo();
-// mazo.mezclar();
-
-// const mano: Carta[] = [];
-
-// /*Este código se utiliza para crear una mano de juego de 5 cartas, 
-// obteniendo las cartas del objeto mazo y agregándolas al arreglo "mano".*/
-// for (let i = 0; i < 5; i++) { //Dentro del bucle que itera 5 veces llama al método dar() del objeto mazo.
-//   const carta = mazo.dar(); //El valor de retorno de dar() se almacena en la constante carta.
-//   if (carta) { //if se asegura de que la constante carta no esta vacío.
-//     mano.push(carta); //Si carta tiene un valor definido, se agrega a la mano del jugador utilizando el método push() del arreglo mano.
-//   }
-// }
-
-//console.log(mano);
